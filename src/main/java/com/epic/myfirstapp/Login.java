4086f0458210ecfe555f99d45e699521dde29ce0
Rahul Satheesh <satheesh_m@epiclanka.net>
2021-06-20 13:09:05 +0530

    initial Commit

---
+++ src/main/java/com/epic/myfirstapp/Login.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epic.myfirstapp;
+
+/**
+ *
+ * @author Satheesh_M
+ */
+public class Login {
+
+    public static void main(String[] args) {
+        String txt = "Hello world";
+        System.out.println(txt.substring(15));
+        int result = 111 / 0;
+
+    }
+}
